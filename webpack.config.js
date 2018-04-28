1028522bc23df3007fe52e93901ea87816e060af
kohheepeace <kohheepeace@kohheepeacenoMacBook.local>
2018-04-28 22:15:29 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
 module.exports = {
   entry: [
-    './src/index.jsx'
+    './src/index.jsx',
   ],
   output: {
-    path: __dirname + '/dist',
+    path: `${__dirname}/dist`,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader', 
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-            },
-          },
-        ],
+        use: ['babel-loader', 'eslint-loader'],
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
 };
